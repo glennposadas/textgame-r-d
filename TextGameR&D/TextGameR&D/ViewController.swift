5244e50a7f7cf8973fba31680d2757932d451f23
Glenn Posadas <hello@glennvon.com>
2020-09-01 01:20:08 +0800

    add first logic for building ui options and answer fields.

--- TextGameR&D/TextGameR&D/ViewController.swift
+++ TextGameR&D/TextGameR&D/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let MAX_OPTIONS_CHAR_LENGTH = 15
 
+class ViewController: UIViewController {
+    
+    private let answerString = "titanic"
+    private var options = [String]()
+    private var fields = [String]()
+    
+    @IBOutlet weak var answerButtonsContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.startLayout()
     }
-
-
+    
+    private func startLayout() {
+        let characters = Array(self.answerString)
+        self.fields = characters.map({ $0.uppercased() })
+        self.options = self.fields
+        
+        let missingOptionsCount = MAX_OPTIONS_CHAR_LENGTH - self.fields.count
+        if missingOptionsCount > 0 {
+            let randomString = self.randomString(length: missingOptionsCount)
+            randomString.forEach { c in
+                self.options.append("\(c)")
+            }
+        }
+        
+        self.options.shuffle()
+        
+        print("answer: \(self.answerString)")
+        print("options: \(self.options)")
+    }
+    
+    private func randomString(length: Int) -> String {
+        let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        return String((0..<length).map{ _ in letters.randomElement()! })
+    }
 }
 
